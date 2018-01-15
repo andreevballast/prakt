9438ed85ae9e74e1725dbfc177ee22bd9b863a31
Alexander <andreev.link@gmail.com>
2018-01-16 00:14:26 +0300

    Done

---
+++ lab_2/lab_2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int n, i, b, c;
+  cin >> n;
+  if (n <= 0)
+  {
+    b = n;
+    c = 1;
+  }
+  else
+  {
+    c = n;
+    b = 1;
+  }
+  int sum = 0;
+  for (i = b; i <= c; i++)
+    sum = sum + i;
+  cout << sum << endl;
+  return 0;
+}
