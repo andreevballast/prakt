d4220120850a4efd167ac540f13b956e4f36af3e
Alexander <andreev.link@gmail.com>
2018-01-16 00:37:24 +0300

    Second commit

--- lab_2/lab_2.cpp
+++ lab_2/lab_2.cpp
@@ -5,19 +5,17 @@ int main()
 {
   int n, i, b, c;
   cin >> n;
-  if (n <= 0)
-  {
+  if (n <= 0) {
     b = n;
     c = 1;
-  }
-  else
-  {
-    c = n;
-    b = 1;
+  } else {
+     c = n;
+     b = 1;
   }
   int sum = 0;
-  for (i = b; i <= c; i++)
+  for (i = b; i <= c; i++) {
     sum = sum + i;
+  }
   cout << sum << endl;
   return 0;
 }
